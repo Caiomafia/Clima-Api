f9095f93682986c57b000149201deaabfd26baee
Caio Mafra <gamersb200@gmail.com>
2022-11-09 22:26:14 -0300

    0000

---
+++ script.js
@@ -0,0 +1,21 @@
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+    minutes = minutes < 10 ? " 0" + minutes : minutes;
+    seconds = seconds < 10 ? " ...
+    display.function startTimer(duration, display) {
+var timer = duration, minutes, seconds;
+setInterval(function () {
+minutes = parseInt(timer / 60, 10);
+function startTimer(duration, display) {
+var timer = duration, minutes, seconds;
+setInterval(function () {
+minutes = parseInt(timer / 60, 10);
+seconds = parseInt(timer % 60, 10);
+minutes = minutes < 10 ? " 0" + minutes : minutes;
+seconds = seconds < 10 ? " ...
+display.minutes = minutes < 10 ? " 0" + minutes : minutes;
+seconds = seconds < 10 ? " ...
+display.
